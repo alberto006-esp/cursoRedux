6a6b5d95f6db90c17d044457a231670b50f3895c
alberto006-esp <albertosuarezolvera@gmail.com>
2022-08-10 14:07:29 +0200

    Crea un almacén Redux

---
+++ CrearAlmacenRedux.js
@@ -0,0 +1,7 @@
+const reducer = (state = 5) => {
+    return state;
+  }
+  
+  // Los métodos Redux están disponibles desde un objeto Redux
+  // Por ejemplo: Redux.createStore()
+  const store = Redux.createStore(reducer);
